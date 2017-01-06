be8bcf4758ecdc9557d683653276b1935df5d61b
17012247 <17012247@e010m08.istic.univ-rennes1.fr>
2017-01-06 12:16:48 +0100

    Failed to execute goal org.apache.maven.plugins:maven-site-plugin:3.3:site (default-site) on project TP1: failed to get report for org.codehaus.mojo:cobertura-maven-plugin: Plugin org.codehaus.mojo:cobertura-maven-plugin:2.7 or one of its dependencies could not be resolved: Could not find artifact com.sun:tools:jar:0 at specified path C:\Program Files (x86)\Java\jre1.8.0_45/../lib/tools.jar -> [Help 1]

---
+++ src/main/java/TP1_SIR/TP1/App.java
@@ -0,0 +1,19 @@
+package TP1_SIR.TP1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    int b;
+    int a;
+    int c;
+    String t;
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
